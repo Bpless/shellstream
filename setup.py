6e1e3a0aee7090b51ab5fcbef8d8b91b9375e690
Ben Plesser <YOUR_NAME@yipit.com>
2013-09-21 10:09:35 -0700

    Bumping the version

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ if __name__ == '__main__':
 
     setup(
         name="shellstream",
-        version='0.0.2.1',
+        version='0.0.2.2',
         description="Python program that pipes your terminal to the cloud",
         long_description=local_file('README.md'),
         author='Benjamin Plesser',
